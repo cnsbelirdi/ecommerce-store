2678a645a48c6e7041f7357e767aa2ee75f1b80e
Cansu Belirdi <cnsbelirdi@gmail.com>
2023-12-15 18:35:59 +0300

    font issue fixed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Urbanist } from "next/font/google";
-import "./globals.css";
+/* import { Urbanist } from "next/font/google";
+ */ import "./globals.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import ModalProvider from "@/providers/modal-provider";
 import ToastProvider from "@/providers/toast-provider";
 
-const font = Urbanist({ subsets: ["latin"] });
+/* const font = Urbanist({ subsets: ["latin"] }); */
 
 export const metadata: Metadata = {
   title: "Store",
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>
+      <body /* className={font.className} */>
         <ModalProvider />
         <ToastProvider />
         <Navbar />
